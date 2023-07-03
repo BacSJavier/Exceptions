de4ae30248de8b65e5b1a3e222f18e385e82bc89
Javier Bacilio Sanchez <l21140750@queretaro.tecnm.mx>
2023-07-02 22:19:17 -0600

    Se ha implementado la excepcion algebraica

--- Flujo.java
+++ Flujo.java
@@ -11,10 +11,15 @@ public class Flujo {
     }
     public static void metodo2() {
         System.out.println("Inicio del metodo 2");
-        for (int i =0 ; i < 5; i++){
-            System.out.println(i);
-        }
+        for (int i =1 ; i < 5; i++){
+            try{
+            int num= i / 0;
+            System.out.println(num);
+            }catch(ArithmeticException exception){
+                System.out.println("A ocurrido una exception aritmetica , el problema fue "+exception.getMessage());
+                exception.printStackTrace();
+            }
         System.out.println("Fin del metodo 2");
     }
-    
+}
 }
