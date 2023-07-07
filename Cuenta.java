efd79eb680b3003be96d7a8d953663c23fc60e0e
Javier Bacilio Sanchez <l21140750@queretaro.tecnm.mx>
2023-07-06 22:07:44 -0600

    Se prepararon las excepciones y se creo el test de cuenta de ahorro

--- Cuenta.java
+++ Cuenta.java
@@ -21,13 +21,13 @@ public abstract class Cuenta {
 
     public abstract void deposita(double valor);
 
-    public boolean saca(double valor) {
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
-            return true;
-        } else {
-            return false;
+    public void saca(double valor) {
+        if(valor > this.saldo) {
+        throw new SaldoInsuficienteException("El saldo es insufuciente no se puede realizar el retiro, intente con otra cantidad");
+        }else{
+        this.saldo -= valor;
         }
+
     }
 
     public boolean transfiere(double valor, Cuenta destino) {
--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
-public static void main(String[] args) {
-    System.out.println("Hola que pasa");   
+public static void main(String[] args)
+{
+    Cuenta test = new CuentaAhorros(1,2);
 }
 }
